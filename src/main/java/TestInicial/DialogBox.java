1d812bc1f3224a32a4bea18b7ed4e70941841be0
lima9 <max9@openmailbox.org>
2016-02-19 19:11:25 +0100

    DialogBOx added

---
+++ src/main/java/TestInicial/DialogBox.java
@@ -0,0 +1,16 @@
+package TestInicial;
+
+/**
+ * Created by lima on 2/18/16.
+ */
+
+import javax.swing.JOptionPane;
+
+public class DialogBox
+{
+    public static void main(String[] args)
+    {
+        JOptionPane.showMessageDialog(null, "My first Box\nIsn't it Great?");
+    }
+
+}
